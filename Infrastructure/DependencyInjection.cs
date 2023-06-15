605ca9e1bd526734cd6e7d9f82a94d5d747c7a36
Jmaxbee <maks5ds@mail.ru>
2023-06-15 11:19:18 +0300

    Initial commit

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        
+        services.AddScoped<IDomainEventService, DomainEventService>();
+        
+        return services;
+    }
+}
---
+++ StaffTimeTableAPI/Program.cs
@@ -0,0 +1,40 @@
+using Application;
+using Infrastructure;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Staff Time-Table",
+        Description = "Demo API - Clean Architecture Solution in .NET 7",
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
