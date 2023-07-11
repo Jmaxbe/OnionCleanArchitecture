038a00ad452ee77531bf006a0fbe76556e6ada95
Jmaxbee <maks5ds@mail.ru>
2023-07-11 15:27:42 +0300

    Added Serilog

--- StaffTimeTableAPI/Program.cs
+++ StaffTimeTableAPI/Program.cs
@@ -1,10 +1,19 @@
-using System.Security.Claims;
 using Application;
 using IdentityServices;
 using Infrastructure;
+using Serilog;
 using StaffTimeTableAPI;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddSerilog();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .WriteTo.Console()
+    .WriteTo.File(builder.Configuration["Logging:LogPath"]!, rollingInterval: RollingInterval.Day)
+    .Enrich.FromLogContext()
+    .CreateLogger();
 
 // Add services to the container.
 builder.Services.AddApplication();
